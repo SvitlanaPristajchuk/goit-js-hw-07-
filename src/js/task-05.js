fb3320d033a0f6e63de5ea640625ce0afdbb36cf
Svetlana Pristaychuk <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2021-09-08 07:27:33 +0300

    fixed 5 task

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -6,9 +6,18 @@ const refs = {
     input: document.querySelector('#name-input'),
     nameLabel: document.querySelector('#name-output'),
 }
-refs.input.addEventListener('input', onInputChange);
+//refs.input.addEventListener('input', onInputChange);
 
-function onInputChange(event){
-    console.log(event.currentTarget.value);
-    return refs.nameLabel.textContent = event.currentTarget.value;
-}
+//function onInputChange(event){
+    //console.log(event.currentTarget.value);
+    //return refs.nameLabel.textContent = event.currentTarget.value;
+//}
+
+refs.input.oninput = function () {
+    if (refs.input.value === "") {
+        refs.nameLabel.innerHTML ="незнакомец";
+    } else {
+        refs.nameLabel.innerHTML = refs.input.value;
+    }
+};
+
